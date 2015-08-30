ab1ee529f3720f01b328a22eaf1218f4a02917da
Gerhard Weis <g.weis@griffith.edu.au>
2015-08-31 09:13:39 +1000

    loop through all log databases for alerts

--- salt/roots/salt/roles/monitor/check_errorlog.sh
+++ salt/roots/salt/roles/monitor/check_errorlog.sh
@@ -5,15 +5,18 @@ export PATH=/usr/local/bin:$PATH
 from="monitor@bccvl.org.au"
 support="g.weis@griffith.edu.au"
 
-# get the error log
-tmpfile=$(mktemp tmp_errorlog.XXXXXXX)
-get_errorlog.sh > "$tmpfile"
+# loop through all log databases:
+for dbname in dev-logs qa-logs prod-logs logs ; do
+    # get the error log
+    tmpfile=$(mktemp tmp_errorlog.XXXXXXX)
+    get_errorlog.sh $dbname > "$tmpfile"
 
-# add email subject header
-sed -i '1iSubject: BCCVL System Alert\n' $tmpfile
+    # add email subject header
+    sed -i '1iSubject: BCCVL System Alert\n' $tmpfile
 
-# email error log to user
-if ! grep "(0 rows)" $tmpfile; then
-    cat $tmpfile | sendmail -f $from $support
-fi
-rm $tmpfile
+    # email error log to user
+    if ! grep "(0 rows)" $tmpfile; then
+        cat $tmpfile | sendmail -f $from $support
+    fi
+    rm $tmpfile
+done
