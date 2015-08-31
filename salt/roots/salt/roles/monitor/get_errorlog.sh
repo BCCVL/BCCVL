1082f7f96c07b6d48a9fd26b01516c09fd0ed00b
Gerhard Weis <g.weis@griffith.edu.au>
2015-08-31 10:34:36 +1000

    use login shell to run sudo command, to avoid 'could not change to directory' error

--- salt/roots/salt/roles/monitor/get_errorlog.sh
+++ salt/roots/salt/roles/monitor/get_errorlog.sh
@@ -11,7 +11,7 @@ username="postgres"
 # Look for any error log with priority less than 4 for the last 6 minutes,
 # as this script is run every 5 minutes .
 
-sudo -u $username psql $dbname << EOF
+sudo -i -u $username psql $dbname << EOF
 
 select devicereportedtime, facility, priority, fromhost, syslogtag, message from systemevents where  devicereportedtime > (now() - interval '6 minutes') and priority < 4 and (facility = 1 or (facility >= 16 and facility <= 23));
 
