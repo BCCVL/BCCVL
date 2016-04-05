04d746c933da055bca4bab3e96ba2212f80ad628
Gerhard Weis <g.weis@griffith.edu.au>
2016-04-05 14:49:34 +1000

    update r libs

--- downloadrlibs.sh
+++ downloadrlibs.sh
@@ -44,59 +44,74 @@ function get_r_lib() {
   ${SHASUM} "${dest}"  | cut -f1 -d ' ' > "${name}"
 }
 
-RLIBS=(rjson_0.2.13.tar.gz
-       sp_1.0-15.tar.gz
-       raster_2.2-31.tar.gz
-       rgdal_0.8-16.tar.gz
-       rgeos_0.3-4.tar.gz
-       gam_1.09.1.tar.gz
-       Rcpp_0.11.1.tar.gz
-       plyr_1.8.1.tar.gz
-       pROC_1.7.2.tar.gz
-       R2HTML_2.2.1.tar.gz
-       png_0.1-7.tar.gz
-       survival_2.37-7.tar.gz
-       lattice_0.20-29.tar.gz
-       gbm_2.0-8.tar.gz
-       stringr_0.6.2.tar.gz
-       evaluate_0.5.3.tar.gz
-       dismo_0.9-3.tar.gz
-       R.methodsS3_1.6.1.tar.gz
-       R.oo_1.18.0.tar.gz
-       R.utils_1.29.8.tar.gz
-       SDMTools_1.1-20.tar.gz
-       abind_1.4-0.tar.gz
-       RColorBrewer_1.0-5.tar.gz
-       latticeExtra_0.6-26.tar.gz
-       hexbin_1.26.3.tar.gz
-       zoo_1.7-11.tar.gz
-       rasterVis_0.28.tar.gz
-       mda_0.4-4.tar.gz
-       randomForest_4.6-7.tar.gz
-       reshape_0.8.4.tar.gz
+RLIBS=(FNN_1.1.tar.gz
+       KernSmooth_2.23-15.tar.gz
+       MASS_7.3-45.tar.gz
+       Matrix_1.2-3.tar.gz
+       R.methodsS3_1.7.1.tar.gz
+       R.oo_1.20.0.tar.gz
+       R.utils_2.2.0.tar.gz
+       R2HTML_2.3.1.tar.gz
+       RColorBrewer_1.1-2.tar.gz
+       Rcpp_0.12.3.tar.gz
+       SDMTools_1.1-221.tar.gz
+       abind_1.4-3.tar.gz
        biomod2_3.1-64.tar.gz
-       MASS_7.3-33.tar.gz
-       nlme_3.1-117.tar.gz
-       gamlss.dist_4.3-0.tar.gz
-       gamlss.data_4.2-7.tar.gz
-       gamlss_4.3-0.tar.gz
-       intervals_0.15.0.tar.gz
-       xts_0.9-7.tar.gz
-       FNN_1.1.tar.gz
-       spacetime_1.1-1.tar.gz
-       gstat_1.0-19.tar.gz
-       deldir_0.1-6.tar.gz
-       ggplot2_1.0.0.tar.gz
-       digest_0.6.4.tar.gz
-       gtable_0.1.2.tar.gz
-       reshape2_1.4.tar.gz
+       boot_1.3-17.tar.gz
+       class_7.3-14.tar.gz
+       cluster_2.0.3.tar.gz
+       codetools_0.2-14.tar.gz
+       colorspace_1.2-6.tar.gz
+       deldir_0.1-9.tar.gz
        dichromat_2.0-0.tar.gz
-       colorspace_1.2-4.tar.gz
-       munsell_0.4.2.tar.gz
+       digest_0.6.9.tar.gz
+       dismo_1.0-15.tar.gz
+       evaluate_0.8.tar.gz
+       foreach_1.4.3.tar.gz
+       foreign_0.8-66.tar.gz
+       gam_1.12.tar.gz
+       gamlss_4.3-8.tar.gz
+       gamlss.data_4.3-2.tar.gz
+       gamlss.dist_4.3-5.tar.gz
+       gbm_2.1.1.tar.gz
+       ggplot2_2.0.0.tar.gz
+       gridExtra_2.2.0.tar.gz
+       gstat_1.1-2.tar.gz
+       gtable_0.2.0.tar.gz
+       hexbin_1.27.1.tar.gz
+       intervals_0.15.1.tar.gz
+       iterators_1.0.8.tar.gz
        labeling_0.3.tar.gz
-       scales_0.2.4.tar.gz
+       lattice_0.20-33.tar.gz
+       latticeExtra_0.6-28.tar.gz
+       magrittr_1.5.tar.gz
+       mda_0.4-8.tar.gz
+       mgcv_1.8-9.tar.gz
+       munsell_0.4.3.tar.gz
+       nlme_3.1-122.tar.gz
+       nnet_7.3-11.tar.gz
+       pROC_1.8.tar.gz
+       plyr_1.8.3.tar.gz
+       png_0.1-7.tar.gz
        proto_0.3-10.tar.gz
-       gridExtra_2.0.0.tar.gz )
+       randomForest_4.6-12.tar.gz
+       raster_2.5-2.tar.gz
+       rasterVis_0.37.tar.gz
+       reshape_0.8.5.tar.gz
+       reshape2_1.4.1.tar.gz
+       rgdal_1.1-3.tar.gz
+       rgeos_0.3-17.tar.gz
+       rjson_0.2.15.tar.gz
+       rpart_4.1-10.tar.gz
+       scales_0.4.0.tar.gz
+       sp_1.2-2.tar.gz
+       spacetime_1.1-5.tar.gz
+       spatial_7.3-11.tar.gz
+       stringi_1.0-1.tar.gz
+       stringr_1.0.0.tar.gz
+       survival_2.38-3.tar.gz
+       xts_0.9-7.tar.gz
+       zoo_1.7-12.tar.gz )
 
 for lib in ${RLIBS[@]} ; do
   get_r_lib "${lib}"
